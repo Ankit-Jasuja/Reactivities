4bb62aa73b7c99d7e12405ddee382fab089309ff
Ankit Jasuja <jasujaankit91@gmail.com>
2021-05-30 16:27:54 +0530

    fetching data from API,enabling cors policy

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,14 @@ namespace API
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
             services.AddDbContext<DataContext>(options => options.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors(opt=>
+            {
+                opt.AddPolicy("CorsPolicy",policy=>
+                {
+                  policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +58,7 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
