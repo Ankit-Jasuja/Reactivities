528b98944a5bf5c242380166d01fd89258947444
Ankit Jasuja <jasujaankit91@gmail.com>
2021-06-01 14:04:37 +0530

    creating query handler following Mediator and CQRS pattern

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,6 @@
-﻿using Domain;
+﻿using Application.Activities;
+using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -11,23 +13,24 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _dataContext;
+        private readonly IMediator _mediator;
 
-        public ActivitiesController(DataContext dataContext)
+        public ActivitiesController(IMediator mediator)
         {
-            _dataContext = dataContext;
+            _mediator = mediator;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>>GetActivities()
         {
-           return await _dataContext.Activities.ToListAsync();
+           return await _mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _dataContext.Activities.FindAsync(id);
+            // return await _dataContext.Activities.FindAsync(id);
+            return Ok();
         }
     }
 }
