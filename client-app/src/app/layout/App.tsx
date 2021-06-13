4eb09517860363373f1e24894b0ceb3a1934e16b
Ankit Jasuja <jasujaankit91@gmail.com>
2021-06-13 19:54:00 +0530

    using uuid package to create new activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import './styles.css';
 import axios from 'axios';
-import { Container, Header, List } from 'semantic-ui-react';
+import { Container} from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './Navbar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import {v4 as uuid} from 'uuid';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);//setting activity to empty initially,
@@ -38,7 +39,7 @@ function App() {
   function handleCreateorEditActivity(activity: Activity) {
     activity.id ?
       setActivities([...activities.filter(x => x.id !== activity.id), activity]) :
-      setActivities([...activities, activity])
+      setActivities([...activities, {...activity,id:uuid()}])
       setEditMode(false);
       setselectedActivity(activity);
   }
