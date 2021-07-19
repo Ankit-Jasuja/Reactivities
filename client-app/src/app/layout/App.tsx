a95dfc64e82640e939cfcd9ea4990b87186fd5aa
Ankit Jasuja <jasujaankit91@gmail.com>
2021-07-19 16:56:23 +0530

    Moving the homepage outside of nav

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -15,13 +15,20 @@ function App() {
 
   return (
     <>
-      <NavBar />
-      <Container style={{ marginTop: '7em' }}>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/activities' component={ActivityDashboard} />
-        <Route path='/activities/:id' component={ActivityDetails} />
-        <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} /> {/* location key changes,component activity form will reset itself instead of updating,so ensuring the useEffect is executed when the key changes */}
-      </Container>
+      <Route exact path='/' component={HomePage} />
+      <Route
+        path={`/(.+)`}
+        render={() => (
+          <>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} /> {/* location key changes,component activity form will reset itself instead of updating,so ensuring the useEffect is executed when the key changes */}
+            </Container>
+          </>
+        )}
+      />
     </>
   );
 }
