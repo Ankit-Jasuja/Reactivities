b58a2c00c6b26fcf14ee65456b0625fc7765ae07
Ankit Jasuja <jasujaankit91@gmail.com>
2021-07-19 16:15:00 +0530

    Redirect to created/Edit activity after creating/editing a activity

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -2,16 +2,19 @@ import { observer } from 'mobx-react-lite';
 import React, { ChangeEvent } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { Button, Form, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/Store';
+import { v4 as uuid } from 'uuid';
 
 export default observer(function ActivityForm() {
 
+    const history = useHistory();
     const { activityStore } = useStore();
     const { createActivity, updateActivity, loading, loadActivity, loadingInitial } = activityStore;
     const { id } = useParams<{ id: string }>();
+
 
     const [activity, setActivity] = useState({
         id: '',
@@ -28,7 +31,13 @@ export default observer(function ActivityForm() {
     }, [id, loadActivity])
 
     function handleSubmit() {
-        activity.id ? updateActivity(activity) : createActivity(activity);
+        if (activity.id.length === 0) {
+            let newActivity = { ...activity, id: uuid() };
+            createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
+        }
+        else {
+            updateActivity(activity).then(() => history.push(`/activities/${activity.id}`));
+        }
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
