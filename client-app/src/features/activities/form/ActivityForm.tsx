3bca165ea29e522f0e59c28e20fd82da5e2763b8
Ankit Jasuja <jasujaankit91@gmail.com>
2021-09-20 18:11:53 +0530

    yup package for validation in formik

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,11 +3,12 @@ import React, { ChangeEvent } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button, FormField, Label, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/Store';
 import { v4 as uuid } from 'uuid';
-import { Formik,Form,Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 export default observer(function ActivityForm() {
 
@@ -26,6 +27,10 @@ export default observer(function ActivityForm() {
         city: '',
         venue: ''
     });
+
+    const validationSchema = yup.object({
+        title: yup.string().required('The activity title is required')
+    })
 
     useEffect(() => {
         if (id) loadActivity(id).then(activity => setActivity(activity!))//activity! implies activity can be undefined as well
@@ -50,14 +55,22 @@ export default observer(function ActivityForm() {
 
     return (
         <Segment clearing>
-            <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
+            <Formik
+                validationSchema={validationSchema}
+                enableReinitialize
+                initialValues={activity}
+                onSubmit={values => console.log(values)}>
                 {({ handleSubmit }) => (
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='Off'>
-                        <Field placeholder='Title' name='title' />
+                        <FormField >
+                            <Field placeholder='Title' name='title' />
+                            <ErrorMessage name='title' 
+                            render={error => <Label basic color='red' content={error} />} />
+                        </FormField>
                         <Field placeholder='Description' name='description' />
                         <Field placeholder='Category' name='category' />
                         <Field type='Date' placeholder='Date' name='date' />
-                        <Field placeholder='City'  name='city' />
+                        <Field placeholder='City' name='city' />
                         <Field placeholder='Venue' name='venue' />
                         <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                         <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
