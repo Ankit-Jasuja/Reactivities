2915959c8eeb3e226321aa571d485b91bbc0a7cb
Ankit Jasuja <jasujaankit91@gmail.com>
2021-09-26 17:38:22 +0530

    creating token service

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,44 @@
+﻿using Domain;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API.Services
+{
+    public class TokenService
+    {
+
+        public string CreateToken(AppUser user)
+        {
+            //token can have claims 
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,user.UserName),
+                new Claim(ClaimTypes.NameIdentifier,user.Id),
+                new Claim(ClaimTypes.Email,user.Email),
+            };
+
+            //we have to sign the token
+            //sign a token with encrption key
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key"));
+            var credentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
+
+            //describe our token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
