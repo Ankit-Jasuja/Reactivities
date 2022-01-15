e39dfddb828c580b4a44c3b22b296256a681d93d
Ankit Jasuja <jasujaankit91@gmail.com>
2022-01-15 19:01:28 +0530

    Configuring new many to many relationship between activity and user by adding new joined table ActivityAttendee

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -10,6 +10,24 @@ namespace Persistence
         {
 
         }
+
         public DbSet<Activity> Activities { get; set; } //Activities is table name
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ActivityAttendee>(z => z.HasKey(s => new {s.AppUserId,s.ActivityId}));
+
+            builder.Entity<ActivityAttendee>()
+                   .HasOne(u => u.AppUser)
+                   .WithMany(a => a.Activities)
+                   .HasForeignKey(z => z.AppUserId);
+
+            builder.Entity<ActivityAttendee>()
+                    .HasOne(u => u.Activity)
+                    .WithMany(a => a.Attendees)
+                    .HasForeignKey(z => z.ActivityId);
+        }
     }
 }
